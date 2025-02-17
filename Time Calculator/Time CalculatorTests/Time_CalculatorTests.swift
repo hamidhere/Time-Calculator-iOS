88257286883ff2adb3dd78a5236c9496a6896433
H A M I . <hamiddot47@gmail.com>
2025-02-17 20:01:11 +0000

    Initial Commit

---
+++ Time Calculator/Time CalculatorTests/Time_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Time_CalculatorTests.swift
+//  Time CalculatorTests
+//
+//  Created by H A M I . on 17/02/2025.
+//
+
+import Testing
+@testable import Time_Calculator
+
+struct Time_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Time Calculator/Time CalculatorUITests/Time_CalculatorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Time_CalculatorUITestsLaunchTests.swift
+//  Time CalculatorUITests
+//
+//  Created by H A M I . on 17/02/2025.
+//
+
+import XCTest
+
+final class Time_CalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
