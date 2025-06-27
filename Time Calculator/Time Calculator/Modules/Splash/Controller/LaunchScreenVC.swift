00b74963fcfea8c4b3565d5ae825648f61298817
H A M I . <hamiddot47@gmail.com>
2025-06-27 07:40:30 +0100

    Pushing pending file

---
+++ Time Calculator/Time Calculator/Modules/Splash/Controller/LaunchScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time Calculator
+//
+//  Created by H A M I . on 17/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
